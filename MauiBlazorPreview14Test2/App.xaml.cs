0987a9c43daec105b5baeb16fd04d69ddcfccd31
Michael Bond <mbond@ltisolutions.com>
2022-03-16 15:11:13 -0400

    Add project files.

---
+++ MauiBlazorPreview14Test2/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBlazorPreview14Test2;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiBlazorPreview14Test2/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBlazorPreview14Test2;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
